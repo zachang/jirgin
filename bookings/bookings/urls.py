8a8ae15a0c8ed44512002240789463f85ce33f12
zachang <zachangdawuda@gmail.com>
2019-02-20 14:58:52 +0100

    Revert "feat(create-user):"
    This reverts commit fa0ac8fe1aa536395c7604f53a6546373db630f2.

--- bookings/bookings/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from authentication.views import home
-
-urlpatterns = [
-    path('', home),
-    path('auth/api/', include('authentication.urls', namespace='authentication')),
-    path('admin/', admin.site.urls),
-]
