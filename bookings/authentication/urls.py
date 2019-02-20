a74fca38a500b49a8f0a29b21a69483c9bba4e9f
zachang <zachangdawuda@gmail.com>
2019-02-20 13:59:07 +0100

    feat(login-user):
    implements user login feature
    
    [finishes #163977806]

--- bookings/authentication/urls.py
+++ bookings/authentication/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_jwt.views import obtain_jwt_token
 from .views import UserListViewSet
 
 router = routers.DefaultRouter()
@@ -8,4 +9,5 @@ router.register(r'^users', UserListViewSet, basename='users')
 app_name = 'authentication'
 urlpatterns = [
     path('', include(router.urls)),
+    path('login/', obtain_jwt_token),
 ]
