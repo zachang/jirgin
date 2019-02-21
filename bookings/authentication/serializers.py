b3dc48c3fe322f78b80bf7167dc56d5a250af101
zachang <zachangdawuda@gmail.com>
2019-02-21 12:39:05 +0100

    bug(update-profile):
    fix for update user profile
    
    [finishes #164135806]

--- bookings/authentication/serializers.py
+++ bookings/authentication/serializers.py
@@ -44,18 +44,25 @@ class UserModifySerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'first_name', 'last_name','username', 'email')
         extra_kwargs = {
-            'username': {'required': False, 'min_length': 2, 'max_length': 30},
-            'first_name': {'required': False, 'min_length': 2, 'max_length': 100},
-            'last_name': {'required': False,'min_length': 2, 'max_length': 100},
-            'email': {'required': False}
+            'username': {
+                'required': False, 'min_length': 2, 
+                'max_length': 30, 'allow_blank': False
+            },
+            'first_name': {
+                'required': False, 'min_length': 2,
+                'max_length': 100, 'allow_blank': False
+            },
+            'last_name': {
+                'required': False, 'min_length': 2,
+                'max_length': 100, 'allow_blank': False
+            },
+            'email': {'required': False, 'allow_blank': False}
         }
 
-    def create(self, validated_data):
-        user = User(
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
-            username=validated_data['username'],
-            email=validated_data['email'],
-        )
-        user.save()    
-        return user
+    def partial_update(self, instance, validated_data):
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email', instance.email)
+        instance.save()
+        return instance
